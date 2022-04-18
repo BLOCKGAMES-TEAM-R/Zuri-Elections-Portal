43001dc9af465a76c28693f75d2b7c83e8fd4767
Clinton Felix <archimedesfelix1@gmail.com>
2022-04-18 16:22:32 +0100

    contract-instance

---
+++ contracts/NestElections.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.5;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract NestElections {
+
+    constructor() {
+
+    }
+
+    function voteNow() public {
+
+    }
+}
