31b6daa095f965810f790b3c16a1add6eed82fda
Clinton Felix <archimedesfelix1@gmail.com>
2022-04-24 01:35:28 +0100

    first commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+describe("ZuriVote", function () {
+
+    let chairman, teachers, bod, students, ZuriVote, zurivote;
+
+    before(async function () {
+        ZuriVote = await ethers.getContractFactory("ZuriVote");
+        [chairman, teachers, students, bod] = await ethers.getSigners();
+        zurivote = await ZuriVote.deploy(["foo", "bar"], ["bar", "foobar"]);
+    });
+
+    it("chairman should start election", async function () {
+
+        expect(await zurivote.startElection()); //.to.be(owner.address);
+    })
+
+    describe("Vote", function () {
+        // it("should add candidate", async function(){
+        it("should be able to vote", async function () {
+
+            expect(await zurivote.vote(1)); //.to.be(owner.address);
+        })
+    })
+})
